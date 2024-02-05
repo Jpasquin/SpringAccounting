e9660366e28b95488bfe1ce4762d1daf69759f86
Jonathan <jonathandominicpasquin@gmail.com>
2024-02-04 19:58:51 -0500

    added backend

---
+++ backend/src/main/java/com/ljj/springaccounting/SpringaccountingApplication.java
@@ -0,0 +1,13 @@
+package com.ljj.springaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaccountingApplication.class, args);
+	}
+
+}
